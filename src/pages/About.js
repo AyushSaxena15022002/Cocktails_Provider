315996579869f5847b34bf33a1edc1da70a3f91e
AyushSaxena15022002 <ayushsaxena339@gmail.com>
2023-07-06 18:42:30 +0530

    Completed

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolorem
+        eveniet consequuntur fugit soluta perferendis amet excepturi omnis
+        facere, doloremque illo reiciendis sed ducimus eligendi quos quo in
+        ullam, dolores neque dolore porro dignissimos animi! Molestias saepe
+        inventore quia accusamus nihil vitae, asperiores tenetur ipsam incidunt
+        voluptatibus vel perspiciatis possimus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
